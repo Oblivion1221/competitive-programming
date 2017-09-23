46c6d7a51e7848bdc9f88750170c59f64170f781
Oblivion1221 <oblivion1221@hotmail.com>
2017-09-23 00:40:45 -0400

    4kyu!!!!

--- codewars/Rust/4kyu_3.rs
+++ codewars/Rust/4kyu_3.rs
@@ -36,13 +36,10 @@ fn dbl_linear(n: u32) -> u32 {
 //     let mut num = 0;
 //     let mut u: Vec<f32> = vec![0.0, 1.0];
 //     while n >= num {
-//         // println!("{} {}", res, num);
 //         if (res - 1) % 2 == 0 || (res - 1) % 3 == 0 {
 //             if u.contains(&((res - 1) as f32 / 2.0)) || u.contains(&((res - 1) as f32 / 3.0)) {
-//                 // println!("(res - 1) / 2 = {:?}\n(res - 1) / 3 = {:?}", (res - 1) / 2, (res - 1) / 3);
 //                 u.push(res as f32);
 //                 num += 1;
-//                 // println!("{:?}", u);
 //             }                     
 //         }
 //         res += 1;
