b129e85efba2e02dfa6b24ffaec24cc9e3bd829d
Oblivion1221 <oblivion1221@hotmail.com>
2017-09-25 04:31:37 -0400

    cool

--- codewars/Python/3kyu_1.py
+++ codewars/Python/3kyu_1.py
@@ -1,13 +1,20 @@
 def fib(n):
     """Calculates the nth Fibonacci number"""
-    def fib_iter(a, b, p, q, cnt):
+    def pfib_iter(a, b, p, q, cnt):
         if cnt == 0:
             return b
         elif not (cnt % 2):
-            return fib_iter(a, b, p ** 2 + q ** 2, q ** 2 + 2 * p * q, cnt // 2)
+            return pfib_iter(a, b, p ** 2 + q ** 2, q ** 2 + 2 * p * q, cnt // 2)
         else:
-            return fib_iter(b * q + a * q + a * p, b * p + a * q, p, q, cnt - 1)
+            return pfib_iter(b * q + a * q + a * p, b * p + a * q, p, q, cnt - 1)
 
-    return fib_iter(1, 0, 0, 1, n)
+    def nfib_iter(a, b, cnt):
+        if cnt == 0:
+            return a
+        else:
+            return nfib_iter(b - a, a, cnt + 1)
 
-print(fib(100000))
+    if n >= 0:
+        return pfib_iter(1, 0, 0, 1, n)
+    else:
+        return nfib_iter(0, 1, n)
