363436e9fceec3c18139614aaf7f62bd7c6bfa12
Oblivion1221 <oblivion1221@hotmail.com>
2017-09-28 02:43:40 -0400

    依然tle。。

--- codewars/Python/4kyu_4.py
+++ codewars/Python/4kyu_4.py
@@ -1,10 +1,58 @@
-def green(n):
-    i = 0
-    while n > 0:
-        if (i * i) % (10 ** len(str(i))) == i and i:
-            n -= 1
-            print("n = " + str(n) + " i = " + str(i))
-        i += 1
-    return i - 1
+def green(arg):
+    def automorphic(p):
+        t = 1
+        n = 5
+        k = 1
+        res = [1]
+        for i in range(1, p + 1):
+            size = 2 ** i
+            n = (3 * n ** 2 - 2 * n ** 3) % (10 ** size)
+            for j in range(k, size + 1):
+                m5 = n % (10 ** j)
+                m6 = 10 ** j + 1 - m5
+                [a, b, c] = sorted([t, m5, m6])
+                # print("n = " + str(n) + "\n[t, m5, m6] = " + str([t,m5,m6]))
+                # print("[a, b, c] = " + str([a, b, c]))
+                res.append(b)
+                res.append(c)
+                t = c
+                # print("res = " + str(res))
+        return sorted(list(set(res)))
 
-print(green(100))
+    p = 0
+    while 2 ** p < arg:
+        p += 1
+    
+    res = automorphic(p)
+    # print(res)
+    return res[arg - 1]
+
+# f = open('out.txt', 'w')
+# for i in range(1, 501):
+#     f.write(str(green(i)) + "\n")
+# f.close()
+#
+# print(green(1))
+# print(green(2))         
+# print(green(3))         
+# print(green(12))         
+# print(green(13))         
+# print(green(100))         
+# print(green(110))
+# print(green(1322))         
+# print(green(2544))                 
+# print(green(5000))
+# 
+# 
+# Trivial:
+# 
+# def green(n):
+#     i = 0
+#     while n > 0:
+#         if (i * i) % (10 ** len(str(i))) == i and i:
+#             n -= 1
+#             print("n = " + str(n) + " i = " + str(i))
+#         i += 1
+#     return i - 1
+# 
+# print(green(100))
