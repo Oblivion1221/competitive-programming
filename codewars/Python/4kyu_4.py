13860a15e66dcc1619c4b3d171502bdf3fd6653e
Oblivion1221 <oblivion1221@hotmail.com>
2017-09-28 04:11:14 -0400

    成功啦
    原来是只能算一遍automorpic(13)的 要不然太耗时间了 我要写个博客庆祝

--- codewars/Python/4kyu_4.py
+++ codewars/Python/4kyu_4.py
@@ -1,31 +1,33 @@
-def green(arg):
-    def automorphic(p):
-        t = 1
-        n = 5
-        k = 1
-        res = [1]
-        for i in range(1, p + 1):
-            size = 2 ** i
-            n = (3 * n ** 2 - 2 * n ** 3) % (10 ** size)
-            for j in range(k, size + 1):
-                m5 = n % (10 ** j)
-                m6 = 10 ** j + 1 - m5
-                [a, b, c] = sorted([t, m5, m6])
-                # print("n = " + str(n) + "\n[t, m5, m6] = " + str([t,m5,m6]))
-                # print("[a, b, c] = " + str([a, b, c]))
-                res.append(b)
-                res.append(c)
-                t = c
-                # print("res = " + str(res))
-        return sorted(list(set(res)))
+import functools
 
-    p = 0
-    while 2 ** p < arg:
-        p += 1
+def automorphic(p):
+    t = 1
+    n = 5
+    k = 1
+    res = [1]
+    for i in range(1, p + 1):
+        size = 2 ** i
+        n = (3 * n ** 2 - 2 * n ** 3) % (10 ** size)
+        for j in range(k, size + 1):
+            m5 = n % (10 ** j)
+            m6 = 10 ** j + 1 - m5
+            [a, b, c] = sorted([t, m5, m6])
+            # print("n = " + str(n) + "\n[t, m5, m6] = " + str([t,m5,m6]) + " ")
+            # print("[a, b, c] = " + str([a, b, c]))
+            res.append(b)
+            res.append(c)
+            t = c
+            # print("res = " + str(res))
+    return sorted(list(set(res)))
 
-    res = automorphic(p)
+@functools.lru_cache(maxsize=1)
+def green_t():
+    res = automorphic(13)
     # print(res)
-    return res[arg - 1]
+    return res
+
+def green(arg):
+    return green_t()[arg - 1]
 
 # f = open('out.txt', 'w')
 # for i in range(1, 501):
