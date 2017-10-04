ecff34f76f20324bcfae4e09fbba9aa41010dbec
Oblivion1221 <oblivion1221@hotmail.com>
2017-10-03 23:55:45 -0400

    呜呜呜

--- codewars/Rust/8kyu_4.rs
+++ codewars/Rust/8kyu_4.rs
@@ -6,6 +6,7 @@ fn repeat_str(src: &str, count: usize) -> String {
   res
 }
 
+// char 和 &str 被 collect 了之后都是 String 啊
 // fn repeat_str(src: &str, count: usize) -> String {
 //   std::iter::repeat(src).take(count).collect()
 // }
